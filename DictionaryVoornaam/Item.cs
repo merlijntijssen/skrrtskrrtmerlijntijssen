a32f6bbbf7839efa5650682cb658dfe254d2ba28
merlijntijssen <merlijntijssen@gmail.com>
2022-12-20 11:54:20 +0100

    opdr dictionary

---
+++ DictionaryVoornaam/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DictionaryVoornaam
+{
+	public class Item
+	{
+		// property
+		public string Description { get; }
+
+		// constructor
+		public Item(string description)
+		{
+			this.Description = description;
+		}
+
+		// methods
+		// ...
+	}
+}
---
+++ DictionaryVoornaam/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryVoornaam
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Documentation: https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.dictionary-2?view=net-6.0
+
+			Dictionary<string, Item> itemDictionary = new Dictionary<string, Item>();
+
+			itemDictionary.Add("hammer", new Item("A heavy hammer"));
+			itemDictionary.Add("axe", new Item("A sharp axe"));
+			itemDictionary.Add("potion", new Item("A bottle labeled 'drink me'"));
+			itemDictionary.Add("medkit", new Item("A medkit"));
+
+            // Voeg een Item toe (Add) met de Key "pill" en description "A bitter pill to swallow"
+            itemDictionary.Add("pill", new Item("A bitter pill to swallow"));
+
+            // Verwijder (Remove) de entry "medkit" uit de dictionary
+            itemDictionary.Remove ("medkit");
+
+            // Bewijs met behulp van code dat er een entry is met de Key "potion". Laat de description zien van dat Item.
+            if (itemDictionary.ContainsKey ("potion"))
+            {
+                Console.WriteLine(itemDictionary["potion"].Description);
+            }
+
+            // Is er een Item "nailgun"? Zo ja, wat is de description? Zo nee, zeg beleefd dat je het Item niet kunt vinden.
+            if (itemDictionary.ContainsKey("nailgun"))
+            {
+                Console.WriteLine(itemDictionary["nailgun"].Description);
+            }
+            else
+            {
+                Console.WriteLine("You dont have this item...");
+            }
+
+            // Laat zien hoeveel Items de itemDictionary bevat (gebruik Count)
+           Console.WriteLine(itemDictionary.Count);
+
+            // Console.WriteLine alle items. Laat zowel de Key zien, als de description van elk Item (Value).
+            foreach(KeyValuePair<string, Item> entry in itemDictionary)
+            {
+                Console.WriteLine("Key = {0}, Description: {1}", entry.Key, entry.Value.Description);
+            }
+
+            // Maak de volledige Dictionary leeg (Clear)
+            itemDictionary.Clear();
+
+            // Laat nogmaals zien hoeveel Items de itemDictionary bevat (gebruik Count)
+            Console.WriteLine(itemDictionary.Count);
+
+        }
+	}
+}
